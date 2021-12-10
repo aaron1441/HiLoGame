ffcfd4c48ff7858b257a3c695244543ee1bc0adc
Aaron Dee <aarondee2014@gmail.com>
2021-12-10 08:39:20 +0000

    Finished the game

--- HiLoGame/HiLoGame/MainPage.xaml.cs
+++ HiLoGame/HiLoGame/MainPage.xaml.cs
@@ -10,6 +10,15 @@ namespace HiLoGame
 {
     public partial class MainPage : ContentPage
     {
+        // global variables
+        Random random;
+        const string GAMES_PLAYED_TEXT = "Games Played: ";
+        const string GAMES_WON_TEXT = "Games Won: ";
+        int currentNum;
+        int startNum;
+        int numToCheck;
+        int higherOrLower;
+        int gamesWon = 0;
         public MainPage()
         {
             InitializeComponent();
@@ -17,7 +26,58 @@ namespace HiLoGame
 
         private void ImageButton_Clicked(object sender, EventArgs e)
         {
-
+            currentNum = random.Next(12);
+            checkHighOrLow();
+            LblCurrentNumber.Text = currentNum.ToString();
+            IBNewCard.IsEnabled = false;
+            numToCheck = currentNum;
         }
+
+        private void BtnStartNewGame_Clicked(object sender, EventArgs e)
+        {
+            if (random == null) random = new Random();
+            LblGamesPlayed.Text = GAMES_PLAYED_TEXT + "--";
+            startNum = random.Next(12);
+            numToCheck = startNum;
+            LblCurrentNumber.Text = startNum.ToString();
+            IBNewCard.IsEnabled = true;
+        }
+
+        private void BtnLower_Clicked(object sender, EventArgs e)
+        {
+            higherOrLower = -1;
+            IBNewCard.IsEnabled = true;
+        }
+
+        private void BtnHigher_Clicked(object sender, EventArgs e)
+        {
+            higherOrLower = 1;
+            IBNewCard.IsEnabled = true;
+        }
+
+        private async void checkHighOrLow()
+        {
+            if(higherOrLower == -1 && currentNum < numToCheck)
+            {
+                await DisplayAlert("Correct", "The number " + currentNum + " is lower than " + numToCheck, "OK");
+                gamesWon++;
+                LblGamesWon.Text = GAMES_WON_TEXT + gamesWon.ToString();
+            }
+            else if (higherOrLower == -1 && currentNum > numToCheck)
+            {
+                await DisplayAlert("Wrong", "The number " + currentNum + " is not lower than " + numToCheck, "OK");
+            }
+            else if (higherOrLower == 1 && currentNum > numToCheck)
+            {
+                await DisplayAlert("Correct", "The number " + currentNum + " is higher than " + numToCheck, "OK");
+                gamesWon++;
+                LblGamesWon.Text = GAMES_WON_TEXT + gamesWon.ToString();
+            }
+            else if (higherOrLower == 1 && currentNum < numToCheck)
+            {
+                await DisplayAlert("Wrong", "The number " + currentNum + " is not higher than " + numToCheck, "OK");
+            }
+        }
+        
     }
 }
